2a2fd84e428798b7964378c8e030399fb87fe586
LukGaming <thelukgaming333@gmaail.com>
2021-03-22 13:20:01 -0300

    creating Clients

--- jp-imports/resources/views/clientes/layout.blade.php
+++ jp-imports/resources/views/clientes/layout.blade.php
@@ -9,6 +9,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
@@ -23,45 +24,53 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mr-auto">
 
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Features</a>
-                </li>
-            </ul>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle navbar-brand text-white" href="http://example.com"
-                            id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            {{ Auth::user()->name }}
-                        </a>
-                        <div class="dropdown-menu text-white" style="background-color: #312C99"
-                            aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item text-white" style="background-color: #312C99"
-                                href="#">Configurações</a>
-                            <a class="dropdown-item text-white" style="background-color: #312C99" href="#">Another
-                                action</a>
-                            <form action="{{ route('logout') }}" method="post">
-                                @csrf
-                                <button class="dropdown-item text-white">Sair</button>
-                            </form>
-                        </div>
-                    </li>
-                </ul>
+        @if (Route::has('login'))
+            @auth
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle navbar-brand text-white" href="http://example.com"
+                                    id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                    {{ Auth::user()->name }}
+                                </a>
+                                <div class="dropdown-menu text-white" style="background-color: #312C99"
+                                    aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item text-white" style="background-color: #312C99"
+                                        href="#">Configurações</a>
+                                    <a class="dropdown-item text-white" style="background-color: #312C99" href="#">Another
+                                        action</a>
+                                    <form action="{{ route('logout') }}" method="post">
+                                        @csrf
+                                        <button class="dropdown-item text-white">Sair</button>
+                                    </form>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>            
+            @else
+                <a href="{{ route('login') }}"
+                    class="collapse navbar-collapse justify-content-end text-sm text-white underline nav-link  navbar-brand text-white"
+                    style="padding-right: 10px">Logar-se</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}"
+                        class=" text-sm text-white underline nav-link  navbar-brand text-white"
+                        style="padding-right: 10px">Registrar-se</a>
+                @endif
+            @endauth
             </div>
-        </div>
+        @endif
     </nav>
     <div class="container">
         @yield('conteudo')
-    </div>
-
-
+    </div>    
+</body>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    <<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
@@ -70,6 +79,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
         integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous">
     </script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+
 
 </html>
